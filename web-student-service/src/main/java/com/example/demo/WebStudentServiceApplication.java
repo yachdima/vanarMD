40ad9a3c84c01e987a7353e46cc8ebfcc32b3a8c
sony <sony@VAIO>
2020-01-11 22:55:48 +0200

    spring

---
+++ web-student-service/src/main/java/com/example/demo/WebStudentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStudentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStudentServiceApplication.class, args);
+	}
+
+}
---
+++ web-student-service/src/main/java/com/example/demo/controlers/StudentControler.java
@@ -0,0 +1,37 @@
+package com.example.demo.controlers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentControler {
+	
+	@RequestMapping(value="/students")
+	public List<String> getStudents(@RequestParam(required = false) String word){
+		ArrayList<String> list = new ArrayList<String>();
+		
+		list.add("John Trevolta");
+		list.add("Mary Cay");
+		list.add("Irina Tobin");
+		
+		// ?word=Jo
+		return list
+				.stream()
+				.filter(name -> word != null && word !="" ? name.contains(word):true)
+				.collect(Collectors.toList());
+	}
+	
+	@PostMapping(value="/students/create")
+	public String createStudent(@RequestParam String name) {
+		String student_name = name;
+		System.err.println(student_name);
+		return student_name;
+	}
+}
